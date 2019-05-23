67f2c2c83de9a82fce717bd3373da81fffd2843c
DennisSoemers <d.soemers@gmail.com>
2019-05-23 11:12:33 +0200

    built-in Match and custom Match examples

---
+++ src/experiments/RunCustomMatch.java
@@ -0,0 +1,114 @@
+package experiments;
+
+import game.Game;
+import player.GameLoader;
+import random.RandomAI;
+import search.mcts.MCTS;
+import util.AI;
+import util.AI.SearchLimits;
+import util.Context;
+import util.Move;
+import util.Trial;
+
+/**
+ * An example of a custom implementation of a match between different AIs,
+ * i.e. not using the built-in Match functionality of Ludii.
+ * 
+ * By creating custom implementations of matches/experiments, we can more
+ * easily add our own custom stats to track, and request mvoes from AIs
+ * that do not implement Ludii's abstract AI class. The downside is that
+ * more boilerplate code must be written.
+ * 
+ * See RunLudiiMatch for an example that uses Ludii's built-in Match 
+ * implementation.
+ * 
+ * Note that this example does not provide all of the functionality included
+ * in the built-in Match implementation. For instance, this example will always
+ * use the same agents for the same player number (e.g. Random AI always player
+ * 1, UCT always player 2), whereas Ludii's built-in Match implementation can
+ * rotate through assignments of agents to player numbers.
+ * 
+ * @author Dennis Soemers
+ */
+public class RunCustomMatch
+{
+	
+	//-------------------------------------------------------------------------
+	
+	/** Name of game we wish to play */
+	static final String GAME_NAME = "Amazons.lud";
+	
+	/** Number of games to play */
+	static final int NUM_GAMES = 10;
+
+	/** List of agents for playing the match */
+	static final AI[] AGENTS = new AI[]{
+			null,
+			new RandomAI(),
+			MCTS.createUCT()
+	};
+
+	//-------------------------------------------------------------------------
+
+	/**
+	 * Constructor
+	 */
+	private RunCustomMatch()
+	{
+		// do not instantiate
+	}
+
+	//-------------------------------------------------------------------------
+
+	public static void main(final String[] args)
+	{
+		// load and create game
+		final Game game = GameLoader.loadGameFromName(GAME_NAME);
+		game.create();
+
+		final Trial trial = new Trial(game);
+		final Context context = new Context(game, trial);
+		
+		for (int gameCounter = 0; gameCounter < NUM_GAMES; ++gameCounter)
+		{
+			// play a game
+			game.start(context);
+			
+			// in this example, we're still using agents that extend Ludii's
+			// abstract AI class, and therefore we call initAI() and 
+			// selectAction() on them
+			//
+			// note that it is also possible to use different kinds of
+			// agents which do not extend this class, and call whatever methods
+			// you like on them
+			for (final AI ai : AGENTS)
+			{
+				if (ai != null)
+				{
+					ai.initAI(game);
+				}
+			}
+			
+			while (!context.trial().over())
+			{
+				final int mover = context.trial().state().mover();
+				final AI ai = AGENTS[mover];
+				final Move move = ai.selectAction
+						(
+							game, 
+							new Context(context),
+							SearchLimits.Seconds, 
+							1.0
+						);
+
+				// apply chosen action
+				game.apply(context, move);
+			}
+			
+			System.out.println("Outcome = " + context.trial().status());
+		}
+	}
+
+	//-------------------------------------------------------------------------
+
+}
---
+++ src/experiments/RunLudiiMatch.java
@@ -0,0 +1,76 @@
+package experiments;
+
+import java.util.Arrays;
+
+import game.Game;
+import player.GameLoader;
+import player.experiments.Match;
+import random.RandomAI;
+import search.mcts.MCTS;
+import util.AI;
+import util.AI.SearchLimits;
+
+/**
+ * Example of an experiment that uses Ludii's built-in Match class to
+ * run games between AIs.
+ * 
+ * See RunCustomMatch for an example that does not use Ludii's built-in Match
+ * implementation.
+ * 
+ * @author Dennis Soemers
+ */
+public class RunLudiiMatch
+{
+	
+	//-------------------------------------------------------------------------
+	
+	/** Name of game we wish to play */
+	static final String GAME_NAME = "Amazons.lud";
+	
+	/** Whether to create a small GUI that can be used to manually interrupt the experiment */
+	static final boolean USE_GUI = false;
+	
+	/** Wall-time limit in minutes (-1 for no limit) */
+	static final int MAX_WALL_TIME = -1;
+	
+	/** List of agents for playing the match */
+	static final AI[] AGENTS = new AI[]{
+			new RandomAI(),
+			MCTS.createUCT()
+	};
+	
+	//-------------------------------------------------------------------------
+	
+	/**
+	 * Constructor
+	 */
+	private RunLudiiMatch()
+	{
+		// do not instantiate
+	}
+	
+	//-------------------------------------------------------------------------
+	
+	public static void main(final String[] args)
+	{
+		// load and create game
+		final Game game = GameLoader.loadGameFromName(GAME_NAME);
+		game.create();
+		
+		// set up our match
+		final Match match = 
+				new Match(USE_GUI, MAX_WALL_TIME)
+				.setGameName(GAME_NAME)
+				.setAgents(Arrays.asList(AGENTS))
+				.setNumGames(10)
+				.setLimitType(SearchLimits.Seconds)
+				.setLimitValue(1.0)
+				.setRotateAgents(true);
+		
+		// start playing
+		match.startMatch();
+	}
+	
+	//-------------------------------------------------------------------------
+
+}
